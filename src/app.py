423b1a1ac5189026b0e007e820d50d0e8597fc46
Orkney06 <kome8728@gmail.com>
2022-03-18 18:19:45 +0900

    daytime and night classification

--- src/app.py
+++ src/app.py
@@ -8,30 +8,37 @@ import tkinter.ttk
 import classification
 
 
-def dirdialog_clicked():
+def dirdialog1_clicked():
     iDir = os.path.abspath(os.path.dirname(__file__))
     iDirPath = tk.filedialog.askdirectory(initialdir = iDir)
-    entry.set(iDirPath)
+    entry1.set(iDirPath)
+
+
+def dirdialog2_clicked():
+    iDir = os.path.abspath(os.path.dirname(__file__))
+    iDirPath = tk.filedialog.askdirectory(initialdir = iDir)
+    entry2.set(iDirPath)
 
 
 def conductMain():
     text = ""
 
-    dirPath = entry.get()
-    if dirPath:
-        text += "フォルダのパス:" + dirPath + '\n'
+    SDirPath = entry1.get()
+    EDirPath = entry2.get()
+    if SDirPath and EDirPath:
+        text += "フォルダのパス:" + SDirPath + '\n'
+        text += "フォルダのパス:" + EDirPath + '\n'
 
     if text:
-        #tk.messagebox.showinfo("info", text)
-        classification.hello(dirPath)
-
+        classification.daytime_or_night(SDirPath, EDirPath)
+        tk.messagebox.showinfo("お知らせ", "画像の分類が終わりました")
     else:
-        tk.messagebox.showinfo("error", "フォルダを指定してください")
+        tk.messagebox.showinfo("エラー", "フォルダを指定してください")
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("画像分類")
+    root.title("日中と夜の画像分類")
 
     frame1 = tk.ttk.Frame(root, padding=10)
     frame1.grid(row=0, column=1, sticky=E)
@@ -43,12 +50,20 @@ if __name__ == "__main__":
     frame3.grid(row=5, column=1, sticky=W)
 
 
-    entry = tk.StringVar()
-    IDirEntry = tk.ttk.Entry(frame1, textvariable=entry, width=30)
+    entry1 = tk.StringVar()
+    IDirEntry = tk.ttk.Entry(frame1, textvariable=entry1, width=30)
     IDirEntry.pack(side=LEFT)
 
-    IDirButton = tk.ttk.Button(frame1, text="フォルダの選択", command=dirdialog_clicked)
+    IDirButton = tk.ttk.Button(frame1, text="入力フォルダの選択", command=dirdialog1_clicked)
     IDirButton.pack(side=LEFT)
+
+    entry2 = tk.StringVar()
+    IDirEntry = tk.ttk.Entry(frame2, textvariable=entry2, width=30)
+    IDirEntry.pack(side=LEFT)
+
+    IDirButton = tk.ttk.Button(frame2, text="出力フォルダの選択", command=dirdialog2_clicked)
+    IDirButton.pack(side=LEFT)
+
 
     button1 = tk.ttk.Button(frame3, text="実行", command=conductMain)
     button1.pack(fill = "x", padx=30, side = "left")
--- src/classification.py
+++ src/classification.py
@@ -1,3 +1,4 @@
+from email import message
 import os
 import sys
 import tkinter as tk
@@ -9,12 +10,46 @@ import app
 import cv2
 import glob
 import numpy as np
+from PIL import Image
 
-def hello(s):
-  types = ('*.png', '*.gif', '*.jpg')
+def daytime_or_night(s, e):
+  input_dirpath = s
+  output_dirpath = e
+
+
+  types = ('*.png', '*.gif', '*.JPG')
   img_paths = []
   for t in types:
-      img_paths.extend(glob.glob(os.path.join(s, t)))
-  str(img_paths)
-  img = cv2.imread(str(img_paths))
-  tk.messagebox.showinfo("info", img_paths)
+      img_paths.extend(glob.glob(os.path.join(input_dirpath, t)))
+
+
+  for path in img_paths:
+      img = Image.open(path)
+      if img is None:
+          print('failed to loading image {}.'.format(path))
+          continue
+
+      count = 0
+      for i in range(0, 50):
+          if (img.getpixel((i, i))[0]) == (img.getpixel((i, i))[1]):
+              count += 1
+              
+      
+      if (count == 50):
+          img = img.convert('L')
+
+      if img.mode == 'RGB':
+          dirpath = os.path.join(output_dirpath, 'Daytime')
+      elif img.mode == 'L':
+          dirpath = os.path.join(output_dirpath, 'Night')
+      else:
+          print('image {}: unsupported image type: {}'.format(path, img.mode))
+          continue
+      os.makedirs(dirpath, exist_ok=True)
+
+      save_path = os.path.join(dirpath, os.path.basename(path))
+      img.save(save_path)
+
+
+    
+   
